259ee23585d086ccca616c748fdfba693579c5a5
Alexander Khabarov <sanyaklirik@gmail.com>
2025-12-18 11:48:56 +0400

    недолетевшие импорты

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-
+import { BooksService } from './books.service';
+import { BooksController } from './books.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Book, BookSchema } from './entities/book.entity';
 
